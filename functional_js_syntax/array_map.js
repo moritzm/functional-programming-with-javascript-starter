c12c963ad12b88727e82e2809e00b5b014bc3fd8
Moritz Moldenhauer <moritz.moldenhauer@gmail.com>
2020-12-29 15:43:41 +0100

    My solutions

--- functional_js_syntax/array_map.js
+++ functional_js_syntax/array_map.js
@@ -1,40 +1,72 @@
 // 1. Write a map function to reverse this array:
-const start = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const start = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // your code
+const myStart = start.map(
+  (number, index, array) => array[array.length - index - 1]
+);
+
+console.log(myStart);
 
 // expected output: Array [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
 // ----------------------------------------------------------
 // 2. Write a map function to print the Job: Name:
-const shipMates = [["Mal", "Captain"], ["Wash", "Pilot"], ["Zoey", "1st Mate"], ["Jayne", "Public Relations"]]
+const shipMates = [
+  ["Mal", "Captain"],
+  ["Wash", "Pilot"],
+  ["Zoey", "1st Mate"],
+  ["Jayne", "Public Relations"],
+];
 
 // your code
+const myShipMates = shipMates.map((item) => `${item[1]}: ${item[0]}`);
+console.log(myShipMates);
 
 // expected output: Array ["Captain: Mal", etc...]
 
 // ----------------------------------------------------------
 // 3. Write a map function that prints the name: even|odd
-const awayTeam = ["Picard", "Riker", "Troy", "Data"]
+const awayTeam = ["Picard", "Riker", "Troy", "Data"];
 
 // your code
+const myAwayTeam = awayTeam.map((item) => {
+  if (item.length % 2 === 0) {
+    return `${item}:even`;
+  } else {
+    return `${item}:odd`;
+  }
+});
 
+console.log(myAwayTeam);
 // expected output: Array: ["Picard: even", "Riker: odd", etc...]
 
 // ----------------------------------------------------------
 // 4. Create a multidimensional array of each item and its index in the original Array.
 
-const sci_fi_shows = ['Manedlorian', 'Enterprise', 'Firefly', 'Battlestar Galactica']
+const sci_fi_shows = [
+  "Manedlorian",
+  "Enterprise",
+  "Firefly",
+  "Battlestar Galactica",
+];
 
 // your code
-
+const my_sci_fi_shows = sci_fi_shows.map((item, index) => {
+  return [item, index];
+});
+console.log(my_sci_fi_shows);
 // expected output: Array [['Manedlorian', 0], ['Enterprise', 1], ['Firefly', 2], ['Battlestar Galactica', 3]]
 
 // ----------------------------------------------------------
 // 5. For each item in this array, create a multidimensional array containing the entire original array.
 
-const sci_fi_shows = [1, 2, 3, 4]
+const sci_fi_shows = [1, 2, 3, 4];
 
 // your code
+const my_sci_fi_shows = sci_fi_shows.map((item, index, array) => {
+  return array;
+});
 
+console.log(my_sci_fi_shows);
 // expected output: Array [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]
