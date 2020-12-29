a29dba479f49440ffdb556fd071def64271af6f1
Moritz Moldenhauer <moritz.moldenhauer@gmail.com>
2020-12-29 11:47:07 +0100

    Udemy solution

--- intro_to_fp/basic_functional_examples.js
+++ intro_to_fp/basic_functional_examples.js
@@ -5,15 +5,26 @@
 // Hint - this will help with time of day: new Date().getHours()
 // -----------------------------------------------------------------
 
-const printText = (time) => {
-  if (time > 12) {
-    return "good afternoon";
-  } else {
-    return "good morning";
-  }
-};
+// const printText = (time) => {
+//   if (time > 12) {
+//     return "good afternoon";
+//   } else {
+//     return "good morning";
+//   }
+// };
 
-console.log(printText(new Date().getHours()));
+// console.log(printText(new Date().getHours()));
+
+function sayHello() {
+  const time = new Date().getHours();
+  if (time > 12 && time < 17) {
+    return "Good Afternoon";
+  }
+
+  return "Good Morning!";
+}
+
+console.log(sayHello());
 
 // -----------------------------------------------------------------
 // Exercise 2
@@ -22,13 +33,24 @@ console.log(printText(new Date().getHours()));
 //       zero.
 // -----------------------------------------------------------------
 
-const giveArray = (number) => {
-  const array = [];
-  for (let i = number; i >= 0; i--) {
-    array.push(i);
+// const giveArray = (number) => {
+//   const array = [];
+//   for (let i = number; i >= 0; i--) {
+//     array.push(i);
+//   }
+
+//   return array;
+// };
+
+// console.log(giveArray(12));
+
+function countDown(startNum) {
+  let countDownArray = [];
+  for (let i = startNum; i >= 0; i--) {
+    countDownArray.push(i);
   }
+  return countDownArray;
+}
 
-  return array;
-};
-
-console.log(giveArray(12));
+console.log(countDown(5));
+//expected output: [5, 4, 3, 2, 1, 0]
