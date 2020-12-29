17a9dbafbbca116b43af918a96883402b686bf3b
Moritz Moldenhauer <moritz.moldenhauer@gmail.com>
2020-12-29 10:46:24 +0100

    My solution

--- intro_to_fp/foundation_concepts.js
+++ intro_to_fp/foundation_concepts.js
@@ -1,15 +1,19 @@
 // Directions: Translate from normal function to pure function
-let greeting = 'Hello, '
+// let greeting = 'Hello, '
 
-const greet = (name) => {
-    return greeting += name
-}
+// const greet = (name) => {
+//     return greeting += name
+// }
 
-// this code could be problematic because greeting is in the global scope. 
+const greet = (name, salutation) => {
+  return `${salutation} ${name}`;
+};
+
+// this code could be problematic because greeting is in the global scope.
 // We could not use the greeting a second time, because the greeting variable is changed to contain the name
 
-console.log(greet('Arthur.'))
-console.log(greet('Arthur.'))
-// expected output: 
-//  Hello, Arthur. 
+console.log(greet("Arthur.", "Hello,"));
+console.log(greet("Arthur.", "Hello,"));
+// expected output:
+//  Hello, Arthur.
 //  Hello, Arthur.
