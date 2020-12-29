89981c4887139d1a954215425c7fedebc70b4106
Moritz Moldenhauer <moritz.moldenhauer@gmail.com>
2020-12-29 11:45:45 +0100

    My solution

--- intro_to_fp/basic_functional_examples.js
+++ intro_to_fp/basic_functional_examples.js
@@ -5,14 +5,30 @@
 // Hint - this will help with time of day: new Date().getHours()
 // -----------------------------------------------------------------
 
+const printText = (time) => {
+  if (time > 12) {
+    return "good afternoon";
+  } else {
+    return "good morning";
+  }
+};
 
-
+console.log(printText(new Date().getHours()));
 
 // -----------------------------------------------------------------
 // Exercise 2
-// Directions: Write a pure function that takes in a number and  
-//       returns an array of items counting down from that number to 
+// Directions: Write a pure function that takes in a number and
+//       returns an array of items counting down from that number to
 //       zero.
 // -----------------------------------------------------------------
 
+const giveArray = (number) => {
+  const array = [];
+  for (let i = number; i >= 0; i--) {
+    array.push(i);
+  }
 
+  return array;
+};
+
+console.log(giveArray(12));
