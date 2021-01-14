e238fd3564d5f6e2aa3d1c10480b33b870edbc99
Moritz Moldenhauer <moritz.moldenhauer@gmail.com>
2021-01-14 09:57:16 +0100

    Filter
    My solution

--- functional_js_syntax/array_filter.js
+++ functional_js_syntax/array_filter.js
@@ -1,13 +1,17 @@
 // 1. Find all the words with more than 7 characters
-const words = ['tardis', 'grok', 'frak', 'blaster', 'klingon', 'shepherd']
+const words = ["tardis", "grok", "frak", "blaster", "klingon", "shepherd"];
 
 // expected output: Array ['shepherd']
+const result = words.filter((x) => x === "shepherd");
+console.log(result);
 
 // ----------------------------------------------------------
 // 2. Find all even values
-const words = [12, 13, 14, 15, 16, 17]
+const words = [12, 13, 14, 15, 16, 17];
 
 // expected output: Array [12, 14, 16]
+const result = words.filter((x) => x % 2 === 0);
+console.log(result);
 
 // ----------------------------------------------------------
 // REAL LIFE EXAMPLES
@@ -15,32 +19,32 @@ const words = [12, 13, 14, 15, 16, 17]
 // We often use filter to quickly pull all the items that share a status or other characteristic. For instance, create a list of all the active bounty hunters from the array below:
 
 const hunters = [
-    {
-        name: 'Greedo',
-        universe: 'Star Wars',
-        status: 'active',
-    },
-    {
-        name: 'Boba Fett',
-        universe: 'Star Wars',
-        status: 'inactive',
-    },
-    {
-        name: 'Asajj Ventress',
-        universe: 'Star Wars',
-        status: 'unknown',
-    },
-    {
-        name: 'Zam Wesell',
-        universe: 'Star Wars',
-        status: 'inactive',
-    },
-    {
-        name: 'Jango Fett',
-        universe: 'Star Wars',
-        status: 'active',
-    },
-]
+  {
+    name: "Greedo",
+    universe: "Star Wars",
+    status: "active",
+  },
+  {
+    name: "Boba Fett",
+    universe: "Star Wars",
+    status: "inactive",
+  },
+  {
+    name: "Asajj Ventress",
+    universe: "Star Wars",
+    status: "unknown",
+  },
+  {
+    name: "Zam Wesell",
+    universe: "Star Wars",
+    status: "inactive",
+  },
+  {
+    name: "Jango Fett",
+    universe: "Star Wars",
+    status: "active",
+  },
+];
 
 // expected output: Array [
 //     {
@@ -54,3 +58,6 @@ const hunters = [
 //         status: 'active',
 //     },
 // ]
+
+const myHunters = hunters.filter((x) => x.status === "active");
+console.log(myHunters);
