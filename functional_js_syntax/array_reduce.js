339d8d5d8b35dc3644a616d880173b54a7ef4b3e
Moritz Moldenhauer <moritz.moldenhauer@gmail.com>
2021-01-14 10:29:22 +0100

    Reduce
    My solution

--- functional_js_syntax/array_reduce.js
+++ functional_js_syntax/array_reduce.js
@@ -1,7 +1,14 @@
 // 1. Take this disjointed sentence and turn it into a single string
-const text = ['The ships', 'hung in the sky,', 'much the way', 'that bricks don`t']
+const text = [
+  "The ships",
+  "hung in the sky,",
+  "much the way",
+  "that bricks don`t",
+];
 
 // Your Code Here
+const myText = text.reduce((text, item) => text + " " + item);
+console.log(myText);
 
 // expected output: "The ships hung in the sky, much the way that bricks don't"
 
@@ -9,26 +16,29 @@ const text = ['The ships', 'hung in the sky,', 'much the way', 'that bricks don`
 
 // 2. Return the winning team
 const scores = [
-    {
-        team: 'A',
-        score: 20
-    },
-    {
-        team: 'B',
-        score: 17
-    },
-    {
-        team: 'C',
-        score: 23
-    },
-    {
-        team: 'D',
-        score: 13
-    }
-]
+  {
+    team: "A",
+    score: 20,
+  },
+  {
+    team: "B",
+    score: 17,
+  },
+  {
+    team: "C",
+    score: 23,
+  },
+  {
+    team: "D",
+    score: 13,
+  },
+];
 
 // Your Code Here
-
+const myScore = scores.reduce((total, item) =>
+  total.score > item.score ? total : item
+);
+console.log(myScore.team);
 // expected output: "C"
 
 // ----------------------------------------------------------
@@ -39,24 +49,32 @@ const scores = [
 // star ship
 
 const ships = [
-    {
-        name: 'Serenity',
-        speed: '4.2G',
-    },
-    {
-        name: 'Cylon Raider',
-        speed: '7.5G',
-    },
-    {
-        name: 'Swordfish II',
-        speed: '50G',
-    },
-    {
-        name: 'Tie Fighters',
-        speed: '4100G',
-    }
-]
+  {
+    name: "Serenity",
+    speed: "4.2G",
+  },
+  {
+    name: "Cylon Raider",
+    speed: "7.5G",
+  },
+  {
+    name: "Swordfish II",
+    speed: "50G",
+  },
+  {
+    name: "Tie Fighters",
+    speed: "4100G",
+  },
+];
 
 // Your Code Here
-
+const myShip = ships.reduce((ship, item) => {
+  const previously = parseInt(ship.speed.replace("G", ""));
+  const current = parseInt(item.speed.replace("G", ""));
+  if (previously > current) {
+    return ship;
+  }
+  return item;
+});
+console.log(myShip.name);
 // Expected output: Tie Fighters
