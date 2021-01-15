7904836f1b33913c763ed2cc925c3bc8a087dd57
Moritz Moldenhauer <moritz.moldenhauer@gmail.com>
2021-01-15 11:31:46 +0100

    DOM solution

--- fp_in_js/render_method.js
+++ fp_in_js/render_method.js
@@ -1,35 +1,55 @@
 // Here is the code from the example text
 
-const root = document.getElementById('root')
+const root = document.getElementById("root");
 
 const Welcome = () => {
-    return `Welcome to my Javascript Program!`
-}
+  return `Welcome to my Javascript Program!`;
+};
 
 const App = () => {
-    return `
+  return `
         <h1>${Welcome()}<h1>
         <div> I EXIST! </div>
-    `
-}
-
-const render = root => {
-    root.innerHTML = App()
-}
-
-render(root)
+    `;
+};
 
 // Add a new Menu component that takes in a show argument which is either true or false
 // Show this content if show is true:
-{/* <nav>
+{
+  /* <nav>
     <ul>
         <li>About Us</li>
         <li>Contact Us</li>
         <li>Login</li>
     </ul>
-</nav> */}
+</nav> */
+}
 
 // and this content if show is false:
-{/* <nav>Menu</nav> */ }
+{
+  /* <nav>Menu</nav> */
+}
 
 // Your Code Here
+
+const Menu = (show) => {
+  if (show) {
+    return `
+                <nav>
+                    <ul>
+                        <li>About Us</li>
+                        <li>Contact Us</li>
+                        <li>Login</li>
+                    </ul>
+                </nav>
+            `;
+  }
+  return `<nav>Menu</nav>`;
+};
+
+const render = (root) => {
+  root.innerHTML = App();
+  root.innerHTML += Menu(true);
+};
+
+render(root);
