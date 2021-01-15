f1625fdfc7ab8d8c2dccd4fd7e43b7fab0ff918c
Moritz Moldenhauer <moritz.moldenhauer@gmail.com>
2021-01-15 10:20:17 +0100

    My Solution

--- fp_in_js/hof_2.js
+++ fp_in_js/hof_2.js
@@ -1,17 +1,25 @@
-weapons_with_noises = [
-    	{name: 'Phaser', noise: 'bssszzsssss', universe: 'Star Trek'},
-	{name: 'Blaster', noise: 'Pew Pew', universe: 'Star Wars'},
-	{name: 'Sonic Screwdriver', noise: 'Pew Pew', universe: 'Dr. Who'},
-	{name: 'Lightsaber', noise: 'Pew Pew', universe: 'Star Wars'},
-	{name: 'Noisy Cricket', noise: 'Pew Pew', universe: 'Men in Black'}
-]
+const weapons_with_noises = [
+  { name: "Phaser", noise: "bssszzsssss", universe: "Star Trek" },
+  { name: "Blaster", noise: "Pew Pew", universe: "Star Wars" },
+  { name: "Sonic Screwdriver", noise: "Pew Pew", universe: "Dr. Who" },
+  { name: "Lightsaber", noise: "Pew Pew", universe: "Star Wars" },
+  { name: "Noisy Cricket", noise: "Pew Pew", universe: "Men in Black" },
+];
 
 function weaponsFromUniverse(universe) {
-	// ...Your code here!
+  // ...Your code here!
+  return (weapon) => {
+    const found = weapons_with_noises.find((x) => x.name === weapon);
+    if (found) {
+      return found.universe === universe
+        ? console.log(`used ${weapon}: ${found.noise}`)
+        : console.log(`${weapon} is not a part of the Star Wars universe`);
+    }
+  };
 }
 
 // USAGE
-const useStarWarsWeapon = weaponsFromUniverse('Star Wars')
+const useStarWarsWeapon = weaponsFromUniverse("Star Wars");
 
-useStarWarsWeapon('Blaster') // console logs 'used Blaster: Pew Pew'
-useStarWarsWeapon('Noisy Cricket') // console logs 'Noisy Cricket is not a part of the Star Wars universe'
+useStarWarsWeapon("Blaster"); // console logs 'used Blaster: Pew Pew'
+useStarWarsWeapon("Noisy Cricket"); // console logs 'Noisy Cricket is not a part of the Star Wars universe'
