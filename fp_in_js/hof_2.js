769097cb28cc14c8ffd41016ff59ab39f18c58e0
Moritz Moldenhauer <moritz.moldenhauer@gmail.com>
2021-01-15 10:22:59 +0100

    Udacity solution

--- fp_in_js/hof_2.js
+++ fp_in_js/hof_2.js
@@ -6,16 +6,34 @@ const weapons_with_noises = [
   { name: "Noisy Cricket", noise: "Pew Pew", universe: "Men in Black" },
 ];
 
+// function weaponsFromUniverse(universe) {
+//   // ...Your code here!
+//   return (weapon) => {
+//     const found = weapons_with_noises.find((x) => x.name === weapon);
+//     if (found) {
+//       return found.universe === universe
+//         ? console.log(`used ${weapon}: ${found.noise}`)
+//         : console.log(`${weapon} is not a part of the Star Wars universe`);
+//     }
+//   };
+// }
+
 function weaponsFromUniverse(universe) {
-  // ...Your code here!
-  return (weapon) => {
-    const found = weapons_with_noises.find((x) => x.name === weapon);
-    if (found) {
-      return found.universe === universe
-        ? console.log(`used ${weapon}: ${found.noise}`)
-        : console.log(`${weapon} is not a part of the Star Wars universe`);
+  const useableWeapons = weapons_with_noises.filter(
+    (w) => w.universe == universe
+  );
+
+  const useWeapon = (weaponName) => {
+    const weapon = useableWeapons.find((w) => weaponName == w.name);
+
+    if (weapon) {
+      console.log(`used ${weapon.name}: ${weapon.noise}`);
+    } else {
+      console.log(`${weaponName} is not a part of the ${universe} universe`);
     }
   };
+
+  return useWeapon;
 }
 
 // USAGE
